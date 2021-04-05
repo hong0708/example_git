fab53b3b5efdfcc440126b795b62b5560cbedcde
minki_hong <gaogai2@naver.com>
2021-04-05 23:10:33 +0900

    first commit

--- programmers_2_1/큰수만들기.py
+++ programmers_2_1/큰수만들기.py
@@ -11,10 +11,10 @@ left = len(num) - k
 loc = 0
 
 while left > 0:
-    if left == 1:
-        loc = num.index(max(num[:]))
-    else:
+    if left == 0:
         loc = num.index(max(num[:-left+1]))
+    else:
+        loc = num.index(max(num[:]))
 
     answer += num[loc]
     left -= 1
